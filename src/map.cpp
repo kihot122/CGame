6b02aa4d9d41f0416b606d4dcb6775d61acc0f0a
Urquelle <m.kowalewski@outlook.com>
2019-07-27 22:11:19 +0200

    c_map

---
+++ src/map.cpp
@@ -0,0 +1,12 @@
+#include "map.hpp"
+
+c_Map::c_Map(uint16_t Width, uint16_t Height) : Size{ Width, Height }
+{
+	Matrix = new c_Cell * *[Width];
+	for (int i = 0; i < Width; i++)
+		Matrix[i] = new c_Cell * [Height];
+}
+
+uint16_t* c_Map::GetSize() { return Size; }
+
+c_Cell* c_Map::GetCell(uint16_t X, uint16_t Y) { return Matrix[X][Y]; }
---
+++ src/map.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <cstdint>
+#include "cell.hpp"
+
+class c_Map
+{
+	c_Cell*** Matrix;
+	uint16_t Size[2];
+public:
+	c_Map(uint16_t Width, uint16_t Height);
+	uint16_t* GetSize();
+	c_Cell* GetCell(uint16_t X, uint16_t Y);
+};
