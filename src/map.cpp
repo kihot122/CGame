0bed682844ea029f042a53e00fa22bebe5db9521
Urquelle <m.kowalewski@outlook.com>
2019-07-28 13:43:32 +0200

    map hotfix

--- src/map.cpp
+++ src/map.cpp
@@ -4,7 +4,11 @@ c_Map::c_Map(uint16_t Width, uint16_t Height) : Size{ Width, Height }
 {
 	Matrix = new c_Cell * *[Width];
 	for (int i = 0; i < Width; i++)
+	{
 		Matrix[i] = new c_Cell * [Height];
+		for (int j = 0; j < Height; j++)
+			Matrix[i][j] = new c_Cell();
+	}
 }
 
 uint16_t* c_Map::GetSize() { return Size; }
